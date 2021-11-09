37d6362056220246feebd395cc147a2e4465efb0
Angel69p <88698508+Angel69p@users.noreply.github.com>
2021-11-09 14:57:47 -0500

    Update Operaciones.java

--- src/Controlador/Operaciones.java
+++ src/Controlador/Operaciones.java
@@ -3,5 +3,38 @@ package Controlador;
 
 
 public class Operaciones {
+         //Valida si es una cadena que se escribo
+    public boolean Estexto(String cad) {
+        boolean sw = true;
+        int lc = cad.length(), i;
+        i = 0;
+        char simbolo;
+        while ((i < lc) && (sw)) {
+            simbolo = cad.charAt(i);
+            if (!(Character.isLetter(simbolo) || Character.isWhitespace(simbolo))) {
+                sw = false;
+            }
+            i++;
+        }
+        return (sw);
+    }  
+     public boolean Esnumero(String cadena) {
+        if (cadena.matches("[0-9]+")) {
+            return (true);
+        } else {
+            return (false);
+        }
+    }
+    /**
+     public boolean EsContraseña(String cadena) {
+        if (cadena.matches("[0-9]+")) {
+            return (true);
+        } else {
+            return (false);
+        }
+    }
+    *//
+     
     
+
 }
